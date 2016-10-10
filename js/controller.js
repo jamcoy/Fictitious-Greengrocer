0d555328165e74e31a7f8e3bb04576a546fc3dc6
jamcoy <james@coynemail.net>
2016-10-10 17:26:39 +0100

    Refactor newsletterForm to user more appropriate ng-model values

--- js/controller.js
+++ js/controller.js
@@ -3,29 +3,23 @@ angular.module('RouteControllers', [])
         $scope.title = "Welcome to the Fictitious Greengrocer";
     })
     .controller('NewsController', function($scope) {
-
         $scope.newsletterUser = {};
-
         $scope.submitForm = function() {
             if ($scope.newsletterForm.$valid) {
-                $scope.newsletterUser.username = $scope.user.username;
-                $scope.newsletterUser.email = $scope.user.email;
+                $scope.newsletterUser.username = $scope.subscriber.name;
+                $scope.newsletterUser.email = $scope.subscriber.email;
             }
-
             console.log($scope.newsletterUser.username, $scope.newsletterUser.email);
         }
     })
     .controller('BoxesController', function($scope) {
-
         $scope.boxCustomer = {};
-
         $scope.submitForm = function() {
             if ($scope.fruitboxForm.$valid) {
                 $scope.boxCustomer.name = $scope.customer.name;
                 $scope.boxCustomer.email = $scope.customer.email;
                 $scope.boxCustomer.phone = $scope.customer.phone;
             }
-
             console.log($scope.boxCustomer.name, $scope.boxCustomer.email, $scope.boxCustomer.phone);
         }
     })
